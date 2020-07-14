f4cda550a2777754b32bdae00ce5d875a6f3865b
lucasmallmann <lucasmallmann76@gmail.com>
2020-07-14 10:10:57 -0300

    test sw.js file

---
+++ public/test.js
@@ -0,0 +1,11 @@
+const truepush = window.truepush || [];
+truepush.push(function () {
+  truepush.Init(
+    {
+      id: "5f0dadc11ad6c54747a6dd39",
+    },
+    function (error) {
+      if (error) console.error(error);
+    }
+  );
+});
