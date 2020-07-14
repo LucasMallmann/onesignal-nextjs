a29ccd677b6bf9ab767e069d2913cd54d37429e5
lucasmallmann <lucasmallmann76@gmail.com>
2020-07-14 19:19:10 -0300

    updated subs func

--- public/script.js
+++ public/script.js
@@ -6,14 +6,7 @@ OneSignal.push(function () {
     appId: "61ccb94d-b9f7-4970-ae11-7c1e40e3b2af",
   });
 
-  // OneSignal.on("subscriptionChange", function (isSubscribed) {
-  //   if (isSubscribed) {
-  //     // The user is subscribed
-  //     //   Either the user subscribed for the first time
-  //     //   Or the user was subscribed -> unsubscribed -> subscribed
-  //     OneSignal.push(function () {
-  //       OneSignal.setExternalUserId(myCustomUniqueUserId);
-  //     });
-  //   }
-  // });
+  OneSignal.on("subscriptionChange", function (isSubscribed) {
+    console.log("changing subscription: ", isSubscribed);
+  });
 });
