80b4f5574a54e276299eb06d80dd4e2c99e3f269
lucasmallmann <lucasmallmann76@gmail.com>
2020-07-14 19:01:25 -0300

    updated custom user id

--- public/script.js
+++ public/script.js
@@ -1,6 +1,19 @@
+const myCustomUniqueUserId = "5f0dedbcbdca4c542c0136a1";
+
 window.OneSignal = window.OneSignal || [];
 OneSignal.push(function () {
   OneSignal.init({
     appId: "61ccb94d-b9f7-4970-ae11-7c1e40e3b2af",
+  });
+
+  OneSignal.on("subscriptionChange", function (isSubscribed) {
+    if (isSubscribed) {
+      // The user is subscribed
+      //   Either the user subscribed for the first time
+      //   Or the user was subscribed -> unsubscribed -> subscribed
+      OneSignal.push(function () {
+        OneSignal.setExternalUserId(myCustomUniqueUserId);
+      });
+    }
   });
 });
