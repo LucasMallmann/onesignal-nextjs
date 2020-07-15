801e52f4b4ce9890d5d60fb8f6fdba384398ac4d
lucasmallmann <lucasmallmann76@gmail.com>
2020-07-15 10:06:10 -0300

    change external id

--- public/script.js
+++ public/script.js
@@ -6,6 +6,10 @@ OneSignal.push(function () {
     appId: "61ccb94d-b9f7-4970-ae11-7c1e40e3b2af",
   });
 
+  OneSignal.getExternalUserId().then((id) =>
+    console.log("The external user id is: ", id)
+  );
+
   OneSignal.on("subscriptionChange", function (isSubscribed) {
     console.log("changing subscription ", isSubscribed);
 
